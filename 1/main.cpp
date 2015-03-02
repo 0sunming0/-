254f602fb9a1fbc9c4daeb101f062601c078e2e4
student <student@studentdeMac-mini-80.local>
2015-03-02 19:08:32 +0800

    Initial Commit

---
+++ 1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by student on 15-3-2.
+//  Copyright (c) 2015年 student sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
